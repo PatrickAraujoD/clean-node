dd1840537c903b36d4122b2e13a571eb054d4c11
PatrickAraujoD <lordpatrick200@gmail.com>
2025-01-22 17:43:37 -0300

    refactor: organize project files

--- src/data/protocols/db/survey-result/save-survey-result-repository.ts
+++ src/data/protocols/db/survey-result/save-survey-result-repository.ts
@@ -1,6 +1,6 @@
 import { SurveyModel } from "@/domain/models/survey";
 import { SurveyResultModel } from "@/domain/models/survey-result";
-import { SaveSurveyResult, SaveSurveyResultModel } from "@/domain/usecases/save-survey-result";
+import { SaveSurveyResult, SaveSurveyResultModel } from "@/domain/usecases/survey-result/save-survey-result";
 
 export interface SaveSurveyResultRepository {
   save (data: SaveSurveyResultModel): Promise<SurveyResultModel>
--- src/data/usecases/load-survey-by-id/db-load-survey-by-id-protocols.ts
+++
@@ -1,4 +0,0 @@
-export * from "@/data/protocols/db/survey/load-survey-by-id-repository";
-export * from "../../protocols/db/survey/load-survey-by-id-repository"
-export * from "@/domain/usecases/load-survey-by-id";
-export * from "@/domain/models/survey"
